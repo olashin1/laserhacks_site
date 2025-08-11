b09161ccf3281317eab02b82bfb544d74b751075
slopefields <yooje44@gmail.com>
2025-08-10 20:36:36 -0700

    Small wording and size changes

--- src/components/FaqSection.jsx
+++ src/components/FaqSection.jsx
@@ -9,13 +9,11 @@ import "../styles/FaqSection.css";
 import accordion_data from "../assets/accordion_data";
 
 export default function FaqSection() {
-  const accordions = accordion_data.map((data, idx) => (
-    <Accordion question={data.question} answer={data.answer} />
-  ));
-  return (
-    <div className="faq-section">
-      <p className="faq-header">Frequently Asked Questions</p>
-      {accordions}
-    </div>
-  );
+    const accordions = accordion_data.map((data, idx) => <Accordion question={data.question} answer={data.answer} />)
+    return (
+        <div className="faq-section">
+            <p className='faq-header'>FAQ</p>
+            {accordions}
+        </div>
+    )
 }
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -6,21 +6,22 @@
 import Card from "./Card";
 import "../styles/Gallery.css";
 
-// TODO: add real images to the images array
 export default function Gallery() {
   const images = [
-    "/image.png",
-    "/image.png",
-    "/image.png",
-    "/image.png",
-    "/image.png",
-    "/image.png",
-  ];
+    "/gallery/IMG_1.png",
+    "/gallery/IMG_2.png",
+    "/gallery/IMG_3.png",
+    "/gallery/IMG_4.png",
+    "/gallery/IMG_5.png",
+    "/gallery/IMG_6.png",
+  ]
   const cards = images.map((imgPath, idx) => <Card key={idx} path={imgPath} />);
   return (
     <>
-      <h3 className="gallery-title">Highlights from Last Time</h3>
-      <div className="gallery-grid">{cards}</div>
+      <h3 className="gallery-title">Highlights from Last Year</h3>
+      <div className="gallery-grid">
+        {cards}
+      </div>
     </>
   );
 }
